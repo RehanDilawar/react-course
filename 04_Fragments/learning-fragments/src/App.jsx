efbb5002b2394fc86fbedab0d280607ae217c525
Rehan Dilawar <hh1styear@gmail.com>
2026-07-19 16:22:48 +0500

    Learning Fragmentation

---
+++ 04_Fragments/learning-fragments/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+function App() {
+  return (
+    // <React.Fragment>
+    <>
+      <h1>Healthy Foods</h1>
+      <ul class="list-group">
+        <li class="list-group-item">Lentils</li>
+        <li class="list-group-item">Green Vegetables</li>
+        <li class="list-group-item">Fruits</li>
+        <li class="list-group-item">Dry Nuts</li>
+        <li class="list-group-item">Eggs</li>
+      </ul>
+    {/* </React.Fragment> */}
+    </>
+  );
+}
+
+export default App;
