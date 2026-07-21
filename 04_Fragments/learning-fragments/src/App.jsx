ffd3260a14681126ad284cf369a0364667b02b56
Rehan Dilawar <hh1styear@gmail.com>
2026-07-21 14:11:58 +0500

    Passing Children to a Component

--- 04_Fragments/learning-fragments/src/App.jsx
+++ 04_Fragments/learning-fragments/src/App.jsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
+import Container from "./components/Container";
 function App() {
   // let foodItems=[]
   let foodItems = [
@@ -11,13 +12,19 @@ function App() {
     "Dry Nuts",
     "Eggs and Milk",
     "Tender Meat",
-    "Oats"
+    "Oats",
   ];
   return (
     <>
-      <h1 className="food-heading">Healthy Foods</h1>
-      <ErrorMessage items={foodItems}></ErrorMessage>
-      <FoodItems items={foodItems}></FoodItems>
+      <Container>
+        <h1 className="food-heading">Healthy Foods</h1>
+        <ErrorMessage items={foodItems}></ErrorMessage>
+        <FoodItems items={foodItems}></FoodItems>
+      </Container>
+      <Container>
+        <p>Above is the list of healthy foods that are good for your body and well
+        being</p>
+      </Container>
     </>
   );
 }
