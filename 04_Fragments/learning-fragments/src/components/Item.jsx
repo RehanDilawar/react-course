62edff1a532c71f09167d64b5d5680a0a209df25
Rehan Dilawar <hh1styear@gmail.com>
2026-07-21 14:31:36 +0500

    Event Catching

--- 04_Fragments/learning-fragments/src/components/Item.jsx
+++ 04_Fragments/learning-fragments/src/components/Item.jsx
@@ -1,11 +1,12 @@
 import css from "./Item.module.css"
 const Item = ({FoodItem}) => {
-  const handlyBuyButtonClicked=(FoodItem)=>{
+  const handlyBuyButtonClicked=(event)=>{
+    console.log(event)
     console.log(`${FoodItem} being bought.`)
   }
   return <li className={`${css['kg-item']} list-group-item kg-item`}><span className={css['kg-span']}>{FoodItem}</span>
   <button className={`${css.button} btn btn-success`}
-  onClick={()=>handlyBuyButtonClicked(FoodItem)}
+  onClick={(event)=>handlyBuyButtonClicked(event)}
   >Buy</button>
   </li>;
 };
