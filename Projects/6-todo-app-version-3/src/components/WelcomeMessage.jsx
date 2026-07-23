466d1dc6ff12d65af96c8b7d3be7321779479db8
Rehan Dilawar <hh1styear@gmail.com>
2026-07-23 22:48:57 +0500

    New Component Created for when no tasks in todo, with message in it

---
+++ Projects/6-todo-app-version-3/src/components/WelcomeMessage.jsx
@@ -0,0 +1,8 @@
+import css from "./WelcomeMessage.module.css"
+const WelcomeMessage = () => {
+  return (
+    <p className={css.message}>Enjoy Your Day!</p>
+  )
+}
+
+export default WelcomeMessage;
