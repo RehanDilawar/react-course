df9623abe76daf1947d20bfabe09b4d3a3f671e7
Rehan Dilawar <hh1styear@gmail.com>
2026-07-23 15:29:58 +0500

    AddTodo Button Functional

--- Projects/6-todo-app-version-3/src/components/AddTodo.jsx
+++ Projects/6-todo-app-version-3/src/components/AddTodo.jsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
 function AddTodo({ onNewItem }) {
+  const [todoName, settodoName] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const handleNameChange=(event)=>{
+    settodoName(event.target.value)
+  }
+  const handleDateChange=(event)=>{
+    setDueDate(event.target.value)
+  }
+  const handleAddButtonClicked=()=>{
+    onNewItem(todoName, dueDate)
+  }
   return (
     <div className="container">
       <div className="row kg-row">
         <div className="col-6">
-          <input type="text" placeholder="Entere Todo here" />
+          <input type="text" placeholder="Entere Todo here" onChange={handleNameChange}/>
         </div>
         <div className="col-4">
-          <input type="date" />
+          <input type="date" onChange={handleDateChange}/>
         </div>
         <div className="col-2">
           <button
             type="button"
             className="btn btn-success kg-button"
-            onClick={() => onNewItem("a", "b")}
+            onClick={handleAddButtonClicked}
           >
             <b>Add</b>
           </button>
