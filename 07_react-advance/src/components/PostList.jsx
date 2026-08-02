8f5139eceb53f8955e8dd2c2c51ce4f0ab9b98c4
Rehan Dilawar <hh1styear@gmail.com>
2026-08-02 21:06:00 +0500

    feat: add LoadingSpinner component and implement asynchronous post fetching in PostList

--- 07_react-advance/src/components/PostList.jsx
+++ 07_react-advance/src/components/PostList.jsx
@@ -1,24 +1,26 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
-
+import LoadingSpinner from "./LoadingSpinner";
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
+  const [dataFetched, setDataFetched] = useState(true);
   useEffect(() => {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
+        setDataFetched(false);
       });
   }, []);
 
   return (
     <>
-      {postList.length === 0 && <WelcomeMessage />}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {dataFetched && <LoadingSpinner />}
+      {!dataFetched && postList.length === 0 && <WelcomeMessage />}
+      {!dataFetched &&
+        postList.map((post) => <Post key={post.id} post={post} />)}
     </>
   );
 };
