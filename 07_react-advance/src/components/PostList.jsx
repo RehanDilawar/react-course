1bdc604e2dab794c871093b8d831bc38374769eb
Rehan Dilawar <hh1styear@gmail.com>
2026-08-02 20:38:40 +0500

    used useEffect hook for efficient fetching of post on first render

--- 07_react-advance/src/components/PostList.jsx
+++ 07_react-advance/src/components/PostList.jsx
@@ -1,21 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
-import { useState } from "react";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
-  const [dataFetched, setDataFetched] = useState(false);
-
-  if (!dataFetched) {
+  useEffect(() => {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
-        setDataFetched(true);
       });
-  }
+  }, []);
 
   return (
     <>
