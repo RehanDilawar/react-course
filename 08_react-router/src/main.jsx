0744ac8820ef9f0149760b4f73af0ad396b46462
Rehan Dilawar <hh1styear@gmail.com>
2026-08-05 22:11:16 +0500

    Set up React Router with basic routing
    Add react-router-dom dependency and configure browser router with two routes: root path for the main App and /create-post for the CreatePost component. Updated main.jsx to use RouterProvider instead of rendering App directly.

--- 08_react-router/src/main.jsx
+++ 08_react-router/src/main.jsx
@@ -1,9 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import CreatePost from "./components/CreatePost.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/create-post",
+    element: <CreatePost />,
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
