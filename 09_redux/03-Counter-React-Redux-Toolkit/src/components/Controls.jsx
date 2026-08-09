8c97bf06c6d77a522dc76483a15c28dd27040c70
Rehan Dilawar <hh1styear@gmail.com>
2026-08-09 22:03:59 +0500

    started redux toolkit

---
+++ 09_redux/03-Counter-React-Redux-Toolkit/src/components/Controls.jsx
@@ -0,0 +1,72 @@
+import React, { useRef } from "react";
+import { useDispatch } from "react-redux";
+
+const Controls = () => {
+  const dispatch = useDispatch();
+  const inputElement = useRef(null);
+
+  const handleIncrement = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+  const handleDecrement = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+  const handlePrivacyToggle = () => {
+    dispatch({ type: "PRIVACY_TOGGLE" });
+  };
+  const handleAdd = () => {
+    dispatch({ type: "ADD", payload: inputElement.current.value });
+    inputElement.current.value = "";
+  };
+  const handleSubtract = () => {
+    dispatch({ type: "SUBTRACT", payload: inputElement.current.value });
+    inputElement.current.value = "";
+  };
+  return (
+    <>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={handleIncrement}
+        >
+          +1
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={handleDecrement}
+        >
+          -1
+        </button>
+        <button
+          type="button"
+          className="btn btn-dark"
+          onClick={handlePrivacyToggle}
+        >
+          Privacy Toggle
+        </button>
+      </div>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center control-row">
+        <input
+          type="number"
+          className="form-control"
+          placeholder="Enter a value"
+          ref={inputElement}
+        />
+        <button type="button" className="btn btn-info" onClick={handleAdd}>
+          Add
+        </button>
+        <button
+          type="button"
+          className="btn btn-warning"
+          onClick={handleSubtract}
+        >
+          Subtract
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Controls;
