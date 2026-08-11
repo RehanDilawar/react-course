0843fb5793e529c9beaa5b212b4fed7c5a1659a9
Rehan Dilawar <hh1styear@gmail.com>
2026-08-12 00:23:16 +0500

    HomeItem: toggle Add/Remove based on bag state
    Read store.bag with useSelector and check if item.id is present. Render a Remove button (dispatches bagActions.removeFromBag) when the item is in the bag; otherwise render the Add button (dispatches bagActions.addToBag). Added elementFound flag, handleRemoveFromBag handler, and imported useSelector to enable the conditional UI.

--- Projects/9-Myntra-Clone/Myntra/src/components/HomeItem.jsx
+++ Projects/9-Myntra-Clone/Myntra/src/components/HomeItem.jsx
@@ -2,11 +2,17 @@ import { useDispatch } from "react-redux";
 import { bagActions } from "../store/bagSlicee";
 import { MdAddShoppingCart } from "react-icons/md";
 import { MdOutlineRemoveShoppingCart } from "react-icons/md";
+import { useSelector } from "react-redux";
 
 const HomeItem = ({ item }) => {
   const dispatch = useDispatch();
+  const bagItems = useSelector((store) => store.bag);
+  const elementFound = bagItems.indexOf(item.id) >= 0;
   const handleAddToCart = () => {
     dispatch(bagActions.addToBag(item.id));
+  };
+  const handleRemoveFromBag = () => {
+    dispatch(bagActions.removeFromBag(item.id));
   };
   return (
     <div className="item-container">
@@ -21,18 +27,25 @@ const HomeItem = ({ item }) => {
         <span className="original-price">Rs {item.original_price}</span>
         <span className="discount">({item.discount_percentage}% OFF)</span>
       </div>
-      <button
-        type="button"
-        className="btn btn-success btn-add-bag"
-        onClick={handleAddToCart}
-      >
-        <MdAddShoppingCart />
-        Add to Bag
-      </button>
-      <button type="button" className="btn btn-danger btn-add-bag">
-        <MdOutlineRemoveShoppingCart />
-        Remove
-      </button>
+      {elementFound ? (
+        <button
+          type="button"
+          className="btn btn-danger btn-add-bag"
+          onClick={handleRemoveFromBag}
+        >
+          <MdOutlineRemoveShoppingCart />
+          Remove
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="btn btn-success btn-add-bag"
+          onClick={handleAddToCart}
+        >
+          <MdAddShoppingCart />
+          Add to Bag
+        </button>
+      )}
     </div>
   );
 };
