c2c2c5c7d3905ec1c2df8d543d40c3163d414329
Rehan Dilawar <hh1styear@gmail.com>
2026-08-11 12:14:01 +0500

    Add fetch status slice and wire into store
    Introduce FetchStatusSlice to track fetchDone and currentlyFetching states (reducers: markFetchDone, markFetchingStarted, markFetchingFinished). Register the new slice in src/store/index.js under fetchStatus. Also clean up commented loader/action lines in src/main.jsx for the Home and Bag routes.

--- Projects/9-Myntra-Clone/Myntra/src/store/index.js
+++ Projects/9-Myntra-Clone/Myntra/src/store/index.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import itemsSlice from "./ItemsSlice";
+import fetchStatusSlice from "./FetchStatusSlice";
 
 const myntraStore = configureStore({
   reducer: {
     items: itemsSlice.reducer,
+    fetchStatus: fetchStatusSlice.reducer,
   },
 });
-
 
 export default myntraStore;
